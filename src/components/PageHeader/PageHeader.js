d072258e8d427218272e17f1bb0223808748d932
Ashith Rahul <ashith.rahul@razorthink.com>
2018-04-13 17:36:06 +0530

    build

--- src/components/PageHeader/PageHeader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './PageHeader.css'
-
-const PageHeader = () =>(
-  <section className={styles.page_Header}>
-dadasasdasdasasasdasdasda
-  </section>);
-export default PageHeader;
